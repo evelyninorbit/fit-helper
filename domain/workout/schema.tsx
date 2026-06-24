b6088d33e72c86d22d03822cf02ae992514785ba
Evelyn <evelyninorbit@gmail.com>
2026-06-25 01:22:15 +0800

    Domain新增workout

---
+++ domain/workout/schema.tsx
@@ -0,0 +1,9 @@
+export type Exercise = {
+    id: number,
+    name: string,
+    unit: string | number
+    bodyPart: string,
+    equipment: string,
+    display: boolean,
+    restTime: number
+}
